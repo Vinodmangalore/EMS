8e7e68cb3378b6b0f11375848d59dfc41f9e737a
Shivanishetty04 <Shivanigshetty04@gmail.com>
2022-12-04 20:50:54 +0530

    i modified the file

--- EmployeeManagementWebApp/Program.cs
+++ EmployeeManagementWebApp/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//comment
 
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
