92746d0a00e4dcff8200daa41642088a19b1cf7f
Shivanishetty04 <Shivanigshetty04@gmail.com>
2022-12-04 20:53:02 +0530

    made some changes in the file

--- EmployeeManagementWebApp/Program.cs
+++ EmployeeManagementWebApp/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //builder.Services.AddRazorPages();
-
+//comment
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
